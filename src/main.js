ba947aa290fe0604fcc1d6d02c93518b8573dc9e
ski <108815730+thelegendski@users.noreply.github.com>
2023-01-24 06:28:07 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,23 +7,30 @@ if the code itself [not the function as that would be a result of bad commentin'
 */
 
 //all presets here
-imageMode(1)
-ellipseMode(1)
-rectMode(1)
-textAlign(1, 1)
+imageMode(1) //center images
+ellipseMode(1) //center circlez
+rectMode(1) //center rectanglez
+textAlign(1, 1) //center text
     
 //all event listeners here
+//objects are faster as they only store the necessary keys.
 const keys = {}, mouse = {}
+//key event listeners
 keyPressed = e => keys[e.key.toLowerCase()] = keys[e.keyCode] = true
 keyReleased = e => keys[e.key.toLowerCase()] = keys[e.keyCode] = false
+//mouve event listeners
 mousePressed = e => mouse[e.button] = true
 mouseReleased = e => mouse[e.button] = false
 mouseClicked = () => mouse.click = true
     
 //all globals here
+//global constants
 const SIZE = 50
+//global arrays
 let dudez = [], blox = [], keyz = [], decor = [], arrowz = [], effex = []
+//global variables
 let scene = 'graphix load'
+//level data an' parser
 const levelz = {
     data: [
         {
@@ -60,6 +67,7 @@ const levelz = {
         //regex parser for spec here
     }
 }
+//all game data
 const game = {
     data: {
         arrows: {
@@ -93,6 +101,7 @@ function arrow (x, y, s, r) {
         endShape(CLOSE)
     popMatrix()
 }
+//graphix object that stores images
 const graphix = {
     player () {
         noStroke()
@@ -105,8 +114,11 @@ const graphix = {
         return get(0, 0, 25, 50)
     }
 }
-let img_index = 0
+let img_index = 0 //index in the array of keys in graphix
+//loads images
 const load = (obj, next='menu') => {
+    //create an array of the keys in obj
+    //a is the key in {a: 1}
     const keys = Object.keys(obj)
     //operator overload, >:)
     
@@ -125,23 +137,35 @@ const load = (obj, next='menu') => {
 //all logic here
 const player = {
     //data
+    //position
     x: 50,
     y: 50,
+    //velocity
     vel: {
+        //x component of velocity vector
         x: 0,
+        //y component of velocity vector
         y: 0,
+        //max value of velocity component
         max: 5
     },
+    //size
     size: 50,
+    //acceleration
     acc: 0.25,
-    timer: 0,
+    //idk anymore
     reset: 10,
+    //health
     health: 100,
-    
-    //functionz
+    //angle
+    ang: 0,
+    //functionz [
+    //draw the player
     draw () {
+        //draws the player image where the player is
         image(graphix.player, this.x, this.y)
     },
+    //moves the player with acceleration an' semi-realistic friction
     move () {
         if(keys[UP]) this.vel.y -= this.acc
         else if(keys[DOWN]) this.vel.y += this.acc
@@ -157,26 +181,92 @@ const player = {
         this.x += this.vel.x
         this.y += this.vel.y
     },
+    //collisions
     collide () {
+        //collide with blox
         blox.forEach(block => {
+            //cache the resolution to conflict
             const cache = col.rcp(this, block)
+            //if the resolution exists, apply it
             cache && ([this.x, this.y] = cache)
         })
-        arrowz.forEach((arrow, i) => col.cc(this, arrow) && arrow.archer !== 'player' && (arrowz.splice(i, 1), this.health -= game.data.arrows[arrow.type].damage))
+        
+        //collide with arrowz
+        arrowz.forEach((arrow, i) => 
+            //if there is a collision
+            col.cc(this, arrow) 
+            //an' the player didn't shoot the arrow
+            && arrow.archer !== 'player' 
+            && (
+                //splice the arrow
+                arrowz.splice(i, 1), 
+                //deal the appropriate 'mount of damage
+                this.health -= game.data.arrows[arrow.type].damage
+            )
+        )
     },
+    //use ranged attack
     shoot () {
-        this.timer-- <= 0 && mouse[0] && (Arrow.create(this.x, this.y, atan2(cam.my - this.y, cam.mx - this.x), 'player', 'basic'), this.timer = this.reset)
+        //draw the bow an' update position/animation
+        this.bow.run(this.x, this.y, this.ang)
+        
+        //if the mouse is pressed
+        mouse[0] 
+        //an' the bow is ready to fire
+        && this.bow.draw.val < 1 
+        //draw the bow
+        && (this.bow.draw.bool = true)
+        
+        //if the bow is fully drawn
+        this.bow.draw.reach 
+        && (
+            //shoot an' arrow where the animated arrow is with the proper specs
+            Arrow.create(this.x + cos(this.ang) * 30, this.y + sin(this.ang) * 30, this.ang, 'player', 'basic'), 
+            //set the bow to not drawn
+            this.bow.draw.reach = false
+        )
     },
+    //use melee attack - add slice attack(?)
+    melee () {
+        //draw an' update the sword
+        this.sword.run(this.x, this.y, this.ang)
+        
+        //if the mouse is pressed
+        mouse[0] 
+        //an' the sword can be thrusted
+        && this.sword.thrust.val < 1 
+        //thrust the sword
+        && (this.sword.thrust.bool = true)
+    },
+    //update weapon/health stat - in dev
+    update (obj) {
+        for(let key in obj){
+            if (typeof obj[key] !== 'object') this[key] = obj[key]
+            else for(let k in obj[key]) this[key][k] = obj[key][k]
+        }
+    },
+    //]
     //wrapper for all functionz
     all () {
+        //update angle
+        this.ang = atan2(cam.my - this.y, cam.mx - this.x)
+        //decide which attack to update
+        this.bow ? this.shoot() : this.melee()
+        //draw the player
         this.draw()
+        //move the player
         this.move()
+        //update player collisions
         this.collide()
-        this.shoot()
-        player.health <= 0 && (scene = 'ded')
+        
+        //if the player is ded
+        player.health <= 0 
+        //um, well they're ded
+        && (scene = 'ded')
     }
 }
 
+//camera object
 const cam = {
     x: 0,
     y: 0,
@@ -226,13 +316,22 @@ class Enemy {
         this.y += sin(this.ang) * this.speed
     }
     collide () {
+        //collide with blox
         blox.forEach(block => {
             const cache = col.rcp(this, block)
             cache && ([this.x, this.y] = cache)
         })
         arrowz.forEach((arrow, i) => col.cc(this, arrow) && arrow.archer === 'player' && (Effex.create(10, arrow), this.health -= game.data.arrows[arrow.type].damage, arrowz.splice(i, 1)))
-        const cache = col.ccp(this, player, 50)
+        let cache = col.ccp(this, player, 50), point
         cache && ([this.x, this.y] = cache)
+        if(player.sword){
+            cache = false
+            if(player.sword.thrust.bool) {
+                cache = col.clp(this, player.sword.pos.a, player.sword.pos.b) ? 'a' : col.clp(this, player.sword.pos.c, player.sword.pos.b) ? 'c' : false
+                if(cache) point = col.cpl(this, player.sword.pos[cache], player.sword.pos.b)
+            }
+            cache && (Effex.create(1, {x: point[0], y: point[1]}, color(0)), this.health -= 5)
+        }
     }
     all () {
         this.ang = atan2(player.y - this.y, player.x - this.x)
@@ -266,7 +365,7 @@ class Arrow {
     constructor (...args) {
         [this.x, this.y, this.ang, this.archer, this.type] = args
         this.speed = 10
-        this.size = 20
+        this.size = 20 * (3 / 4)
     }
     draw () {
         pushMatrix()
@@ -289,11 +388,117 @@ class Arrow {
     }
 }
 
+/*
+should the bow draw back?
+ngl, not sure, but willin' to do anythin' to
+juice this up.
+*/
 class Bow {
-    constructor(obj){
-        this.x = obj.x
+    constructor(...args){
+        [this.x, this.y, this.ang, this.timer] = args
+        //as in you *draw* a bow
+        this.draw = {
+            //if bow is bein' drawn
+            bool: false,
+            //amount bow is bein' drawn
+            val: 0,
+            //max stretch of drawn bow
+            max: 30,
+            //if the bow has reached its max
+            reach: false,
+            arrow: 50
+        }
+    }
+    run (...args) {
+        [this.x, this.y, this.ang] = args
+        pushMatrix()
+            translate(this.x, this.y)
+            rotate(this.ang)
+            noFill()
+            strokeCap(ROUND)
+            strokeWeight(3)
+            stroke(200, 150, 100)
+            curve(30, -30, 60, 0, 30, 30)
+            stroke(150)
+            curve(30, -30, 30 - this.draw.val, 0, 30, 30)
+        if(this.draw.bool){
+            pushMatrix()
+                translate(this.draw.arrow, 0)
+                rotate(90)
+                scale(0.75)
+                image(graphix.arrow, 0, 0)
+            popMatrix()
+            this.draw.val = lerp(this.draw.val, this.draw.max, 60 / (this.timer * 10))
+            this.draw.arrow = lerp(this.draw.arrow, 30, 60 / (this.timer * 10))
+            this.draw.val > this.draw.max - 1 && (this.draw.reach = true, this.draw.arrow = 50, this.draw.bool = false)
+        }
+        else this.draw.val = lerp(this.draw.val, 0, 60 / (this.timer * 10))
+        this.draw.val = constrain(this.draw.val, 0, this.draw.max)
+        
+        popMatrix()
     }
 }
+player.bow = new Bow(player.x, player.y, player.ang, 50)
+
+//melee isn't easy; i think.
+class Sword {
+    constructor (...args) {
+        [this.x, this.y, this.ang, this.timer] = args
+        this.pos = {
+            a: {x: 0, y: 0},
+            A: {
+                x: 30,
+                y: -10
+            },
+            b: {x: 0, y: 0},
+            B: {
+                x: 60,
+                y: 0
+            },
+            c: {x: 0, y: 0},
+            C: {
+                x: 30,
+                y: 10
+            },
+            d: {x: 0, y: 0},
+            D: {
+                x: 30,
+                y: 0
+            }
+        }
+        this.thrust = {
+            bool: false,
+            val: 0
+        }
+    }
+    rotate (key) {
+        key = key.toLowerCase()
+        const point = this.pos[key], pos = this.pos[key.toUpperCase()]
+        const c = cos(-this.ang), s = sin(-this.ang)
+        this.pos[key].x = (c * pos.x) + (s * pos.y) + this.x + cos(this.ang) * this.thrust.val
+        this.pos[key].y = (c * pos.y) - (s * pos.x) + this.y + sin(this.ang) * this.thrust.val
+    }
+    run (...args) {
+        [this.x, this.y, this.ang] = args
+        
+        
+        for(let key in this.pos) key === key.toLowerCase() && this.rotate(key)
+        
+        strokeWeight(1)
+        stroke(125)
+        fill(150)
+        triangle(this.pos.a.x, this.pos.a.y, this.pos.b.x, this.pos.b.y, this.pos.c.x, this.pos.c.y)
+        strokeWeight(2)
+        stroke(225, 100, 100)
+        line(this.pos.a.x, this.pos.a.y, this.pos.b.x, this.pos.b.y)
+        line(this.pos.c.x, this.pos.c.y, this.pos.b.x, this.pos.b.y)
+        
+        
+        if(this.thrust.bool) (this.thrust.val = lerp(this.thrust.val, 30, 60 / (this.timer * 10)), this.thrust.val > 30 - 1 && (this.thrust.bool = false))
+        else this.thrust.val = lerp(this.thrust.val, 0, (60 / (this.timer * 10)))
+    }
+}
+//player.sword = new Sword(0, 0, 0, 25)
 
 //this might be one of the crazier classes
 //the effex class creates a particle effect
@@ -450,3 +655,4 @@ draw = () => {
     mouse.click = false
     }catch(e){println(e.stack); noLoop()}
 }
+//<script>
